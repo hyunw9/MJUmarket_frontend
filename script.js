14605b3a96cc19a6cfca74cfe8e71636a3ee086a
consieun <ja24z8@naver.com>
2022-10-30 03:41:31 +0900

    인증번호확인

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const sendBtn = document.querySelector("#btn-email-send")
 const randomId_ = String(Math.floor(Math.random()*(9-0)+1))+String(Math.floor(Math.random()*(9-0)+1))+String(Math.floor(Math.random()*(9-0)+1))+String(Math.floor(Math.random()*(9-0)+1))+String(Math.floor(Math.random()*(9-0)+1))+String(Math.floor(Math.random()*(9-0)+1));
 
+const checkBtn = document.querySelector("#btn-email-check")
+
 sendBtn.addEventListener('click', ()=>{
     emailjs.init("user_NfITGtYx2H9AaKpXWJUNw");
 
@@ -17,4 +19,17 @@ function sendSuccess(){
 
 function sendFail(error){
     alert(`${error.text} 이러한 이유로 실패했어요`)
+}
+
+checkBtn.addEventListener("click", checkMessage)
+
+function checkMessage(){
+    const checkNumber = document.querySelector("#useremail-ckeck");
+    const message = document.querySelector("#message")
+    if(randomId_ == checkNumber.value){
+    message.innerHTML = "인증 번호가 일치합니다."
+    }
+    else{
+        message.innerHTML = "인증 번호가 틀렸습니다."
+    }
 }
