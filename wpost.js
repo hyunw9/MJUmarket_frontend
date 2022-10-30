666233f563b3dd144a0663a78527409a1c3285f6
sieun <ja24z8@naver.com>
2022-10-31 03:16:10 +0900

    글쓰기

---
+++ wpost.js
@@ -0,0 +1,10 @@
+function submitPost(){
+    const title = document.getElementById("post-title").value
+    const pic = document.getElementById("post-pic").value
+    const price = document.getElementById("price").value
+    const content = document.getElementById("post-body").value
+    window.localStorage.setItem('postTitle', title)
+    window.localStorage.setItem('price', price)
+    window.localStorage.setItem('content', content)
+    window.localStorage.setItem('pic', content)
+}
