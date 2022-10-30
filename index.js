b9035657122901c1b53acd2291902a9698b538b5
ironjoong <choel3676@gmail.com>
2022-10-30 23:42:19 +0900

    변경사항 저장

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function submitData(){
             
   const email = document.getElementById("email");
   const password = document.getElementById('password')
+  
   axios({
       method:"POST",
       url: 'ec2-3-35-149-126.ap-northeast-2.compute.amazonaws.com:8080/auth/login',
