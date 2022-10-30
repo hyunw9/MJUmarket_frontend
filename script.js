584f0ce3d0c328e969dce6d8d78700581eee37be
sieun <ja24z8@naver.com>
2022-10-30 23:13:59 +0900

    asdf

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const randomId_ = String(Math.floor(Math.random()*(9-0)+1))+String(Math.floor(Ma
 const checkBtn = document.querySelector("#btn-email-check")
 
 document.getElementById("btn-email-send").onclick = function(){
-    alert(String(document.getElementById("useremail").value).indexOf("mju.ac.kr", 0))
     if(String(document.getElementById("useremail").value).indexOf("mju.ac.kr", 0)==-1){
         alert("mju.ac.kr을 사용하세요.")
     } else{
