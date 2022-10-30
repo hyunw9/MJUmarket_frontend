e06abd8f72daa102e51aad21d51c5dbf37510a21
sieun <ja24z8@naver.com>
2022-10-31 03:57:54 +0900

    html

--- wpost.js
+++ wpost.js
@@ -1,9 +1,11 @@
 function submitPost(){
     const title = document.getElementById("post-title").value
-    const pic = document.getElementById("post-pic").value
     const price = document.getElementById("price").value
     const content = document.getElementById("post-body").value
+    const now_state = document.getElementById("sold-state").value
+
     window.localStorage.setItem('postTitle', title)
     window.localStorage.setItem('price', price)
     window.localStorage.setItem('content', content)
+    window.localStorage.setItem('state', now_state)
 }
